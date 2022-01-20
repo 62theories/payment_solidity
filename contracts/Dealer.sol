492649f994273107d151f89134f242e1e49abd3c
62theories <s5935512005@phuket.psu.ac.th>
2022-01-20 13:19:24 +0700

    addadmin revokeadmin cancelorder

--- contracts/Dealer.sol
+++ contracts/Dealer.sol
@@ -10,6 +10,7 @@ contract Dealer {
         uint256 amount;
         uint256 price;
         bool isMatched;
+        bool isCanceled;
     }
 
     struct OrderBuy {
@@ -21,14 +22,45 @@ contract Dealer {
     uint256 public lastestOrderId;
     mapping(address => mapping(uint256 => OrderSell)) orderSells;
     mapping(uint256 => OrderBuy) orderBuys;
+    mapping(address => bool) admins;
 
-    constructor() {}
+    constructor(address _admin) {
+        admins[_admin] = true;
+    }
+
+    modifier isAdmin() {
+        require(admins[msg.sender] == true);
+        _;
+    }
+
+    modifier isNotMatched(uint256 orderId) {
+        require(orderSells[msg.sender][orderId].isMatched == false);
+        _;
+    }
+
+    modifier isNotCanceled(uint256 orderId) {
+        require(orderSells[msg.sender][orderId].isCanceled == false);
+        _;
+    }
+
+    modifier isNotPaid(uint256 orderId) {
+        require(orderBuys[orderId].isPaid == false);
+        _;
+    }
+
+    function addAdmin(address _admin) public isAdmin {
+        admins[_admin] = true;
+    }
+
+    function revokeAdmin(address _admin) public isAdmin {
+        admins[_admin] = false;
+    }
 
     function createSellOrder(
         address tokenAddress,
         uint256 amount,
         uint256 price
-    ) public {
+    ) public returns (uint256) {
         IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
         uint256 newOrderId = lastestOrderId++;
         orderSells[msg.sender][newOrderId] = OrderSell({
@@ -36,12 +68,27 @@ contract Dealer {
             tokenName: IERC20(tokenAddress),
             amount: amount,
             price: price,
-            isMatched: false
+            isMatched: false,
+            isCanceled: false
         });
+        return newOrderId;
     }
 
-    function matchOrder(uint256 orderId) public {
-        require(orderSells[msg.sender][orderId].isMatched == false);
+    function cancelOrder(uint256 orderId)
+        public
+        isNotMatched(orderId)
+        isNotCanceled(orderId)
+    {
+        orderSells[msg.sender][orderId].isCanceled = true;
+    }
+
+    function matchOrder(uint256 orderId)
+        public
+        isAdmin
+        isNotMatched(orderId)
+        isNotCanceled(orderId)
+        returns (uint256)
+    {
         orderSells[msg.sender][orderId].isMatched = true;
         uint256 newOrderId = lastestOrderId++;
         orderBuys[newOrderId] = OrderBuy({
@@ -49,9 +96,10 @@ contract Dealer {
             matchedOrderId: orderId,
             isPaid: false
         });
+        return newOrderId;
     }
 
-    function payFinish(uint256 orderId) public {
+    function payFinish(uint256 orderId) public isNotPaid(orderId) {
         orderBuys[orderId].isPaid = true;
     }
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,30 +1,68 @@
-const { expect } = require("chai");
+const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 
-// describe("Greeter", function () {
-//   it("Should return the new greeting once it's changed", async function () {
-//     const Greeter = await ethers.getContractFactory("Greeter");
-//     const greeter = await Greeter.deploy("Hello, world!");
-//     await greeter.deployed();
+let myTokenContract = null;
+let dealerContract = null;
 
-//     expect(await greeter.greet()).to.equal("Hello, world!");
-
-//     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-//     // wait until the transaction is mined
-//     await setGreetingTx.wait();
-
-//     expect(await greeter.greet()).to.equal("Hola, mundo!");
-//   });
-// });
-
-describe("MyToken", function () {
-  let tokenAddress = "";
-
+describe("deploy contracts", async function () {
   it("Deploy MyToken", async function () {
+    const [deployer, tokenOwner] = await ethers.getSigners();
     const MyToken = await ethers.getContractFactory("MyToken");
-    const myToken = await MyToken.deploy("Token", "TK");
-    const tokenAddress = (await myToken.deployed()).address;
-    console.log("tokenAddress ", tokenAddress);
+    myTokenContract = await MyToken.deploy("Token", "TK", tokenOwner.address);
+    expect(myTokenContract.address.length).to.not.equal(0);
+  });
+
+  it("Deploy Dealer", async function () {
+    const [deployer, tokenOwner, adminOne] = await ethers.getSigners();
+    const dealer = await ethers.getContractFactory("Dealer");
+    dealerContract = await dealer.deploy(adminOne.address);
+    expect(dealerContract.address.length).to.not.equal(0);
+  });
+});
+
+describe("sell order", async function () {
+  it("When customer sell customer balance decreased + dealer balance increase", async function () {
+    const [deployer, tokenOwner] = await ethers.getSigners();
+    const prevCusBalance = await myTokenContract
+      .connect(tokenOwner)
+      .balanceOf(tokenOwner.address);
+    const prevTokenOwnerBalance = await myTokenContract
+      .connect(tokenOwner)
+      .balanceOf(dealerContract.address);
+    await myTokenContract
+      .connect(tokenOwner)
+      .approve(dealerContract.address, 10);
+    await dealerContract
+      .connect(tokenOwner)
+      .createSellOrder(myTokenContract.address, 10, 5);
+    const newCusBalance = await myTokenContract
+      .connect(tokenOwner)
+      .balanceOf(tokenOwner.address);
+    const newTokenOwnerBalance = await myTokenContract
+      .connect(tokenOwner)
+      .balanceOf(dealerContract.address);
+    assert(prevCusBalance.sub(newCusBalance).eq(ethers.BigNumber.from(10)));
+    assert(
+      newTokenOwnerBalance
+        .sub(prevTokenOwnerBalance)
+        .eq(ethers.BigNumber.from(10))
+    );
+  });
+
+  it("admin can match sell order", async function () {
+    const [deployer, tokenOwner, adminOne] = await ethers.getSigners();
+    await myTokenContract
+      .connect(tokenOwner)
+      .approve(dealerContract.address, 10);
+    const sellOrderId = await dealerContract
+      .connect(tokenOwner)
+      .createSellOrder(myTokenContract.address, 10, 5);
+    // must use event
+    // console.log(sellOrderId);
+    // assert(sellOrderId.length > 0);
+    // const buyOrderId = await dealerContract
+    //   .connect(adminOne)
+    //   .matchOrder(sellOrderId);
+    // assert(buyOrderId.length > 0);
   });
 });
